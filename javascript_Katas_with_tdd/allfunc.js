ae53684cf35fe16e4c2eaa24fee2c94be04d8336
epic0448 <samuelogundiran@Yahoo.com>
2020-12-01 07:40:46 -0500

    all test are passing

--- javascript_Katas_with_tdd/allfunc.js
+++ javascript_Katas_with_tdd/allfunc.js
@@ -7,7 +7,28 @@ const allFunc = () => {
     const jump = (str) => {
         return str.split()
     }
- return {sum, jump}
+   
+    
+    const useMatch = (a,b) => {
+     return  a + b
+    }
+    
+    const assigndata = (assignData) => {
+        //Should not be passing.
+
+
+        // assignData = { one: 1 }; 
+        // assignData['two'] = 2; 
+       return assignData
+    }
+
+    const checkForBeer = (shoppingList) => {
+        //Should not be passing.
+
+        return null
+    }
+    
+  return { sum, jump, useMatch,assigndata,checkForBeer }
 }
  
 module.exports = allFunc
--- javascript_Katas_with_tdd/allfunc.test.js
+++ javascript_Katas_with_tdd/allfunc.test.js
@@ -7,6 +7,7 @@ describe("allFunc", () => {
   it("should return the sum of the input a and b", () => {
     const call = allFunc()
     const result = call.sum(1, 2)
+     // to get the opposite. you say [expect(result).not.toBe(0)
     expect(result).toEqual(3)
   })
 
@@ -15,36 +16,38 @@ describe("allFunc", () => {
     const result = call.jump("Jump Hight")
     expect(result).toContain("Jump Hight")
   })
-})
 
 
-//const index_start = require('./allfunc');
+  // Using matchers.  (toBe uses Object.is to test exact equality. If you want to check the value of an object, use toEqual instead:)
+
+  it("use matcher to test exact equality  specific Number, in this case 2+2 =4", () => {
+    const call = allFunc()
+    //const result = call.useMatch("num")
+    expect(2 + 2).toBe(4);
+  });
+
+  //toEqual 
+  //(toEqual recursively checks every field of an object or array.)
+  it("object assignment using toEqual", () => {
+    const call = allFunc()
+    const assignData = { one: 1 }; assignData['two'] = 2; 
+    expect(assignData).toEqual({ one: 1, two: 2 });
+   })
 
   
-//     test('adds 1+2 to equal 3', () => {
-//     expect(sum(1, 2)).toBe(3)
-// })
-
-// //  ===group testing.=========
-
-// describe('first set', () => {
-//     // beforeEach(() => {
-//     //   //do something
-//     // })
-//     // afterAll(() => {
-//     //   //do something
-//     // })
-//     test(/*...*/)
-//     test(/*...*/)
-//   })
+    // check for arrays and iterables. 
+    const shoppingList = [
+      'diapers',
+      'kleenex',
+      'trash bags',
+      'paper towels',
+      'beer',
+    ];
   
-//   describe('second set', () => {
-//     beforeEach(() => {
-//       //do something
-//     })
-//     beforeAll(() => {
-//       //do something
-//     })
-//     test(/*...*/)
-//     test(/*...*/)
-//   })
+  it('the shopping should include beer in it', () => {
+    expect(shoppingList).toContain('beer');
+    expect(new Set(shoppingList)).toContain('beer')
+  })
+  
+}) // end of describe
+
